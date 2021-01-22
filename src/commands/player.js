ec0afdbec422e43aae1efe4be7529630e0997195
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-01-22 19:25:21 -0300

    teste de comando de ajuda embed

--- src/commands/player.js
+++ src/commands/player.js
@@ -4,7 +4,7 @@ const rootFunctions = require('./root')
 const { prefix, eyeEmoji } = require('../utils/config.json');
 const { commandsArray, commandsData } = require('../data/commands.js')
 
-const ajuda = require('../data/ajuda.js')
+// const ajuda = require('../data/ajuda.js')
 
 const queue = new Map();
 
@@ -23,7 +23,17 @@ module.exports = {
     const receivedContentString = receivedMessage.content;
 
     if (receivedContentString.startsWith(`${prefix}ajuda`)) {
-      receivedMessage.reply(ajuda);
+        const ajudaEmbed = new MessageEmbed()
+          .setTitle('Ajuda Aqui!!!')
+          .setColor(0x0000FF)
+          .setDescription('Confira os links e resolva todos seus problemas!')
+          .addField('Comandos/Documentação: ', 'https://github.com/LucasGuerraCavalcante/xaropinho-bot/blob/main/README.md')
+          .addField('Link para adicionar o bot: ', 'https://discord.com/oauth2/authorize?client_id=801642174093590538&permissions=3665472&scope=bot')
+          .addField('Repositório GitHub: ', 'https://github.com/LucasGuerraCavalcante/xaropinho-bot')
+          .addField('Dúvidas? Sugestões? Bugs?: ', 'lucasguerratee@gmail.com');
+
+        message.channel.send(ajudaEmbed);
+        // receivedMessage.reply(ajuda);
       return;
     }
 
