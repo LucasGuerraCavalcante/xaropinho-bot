0872f810d92122f54daa2e669c0281834de17ab1
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-01-21 23:42:47 -0300

    express test

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-
+const express = require('express');
 const Discord = require('discord.js');
 
 require('dotenv').config();
 
-const commands = require('./src/commands/player')
-
-// const AUD_BOT_TOKEN = require('./utils/keys')
-// bot_token = AUD_BOT_TOKEN
+const commands = require('./src/commands/player');
 
 const bot_token = process.env.DISCORD_TOKEN;
 
 const client = new Discord.Client();
+
+// -------------------------------------------------------------------------------------------------
 
 client.once('ready', () => {
     console.log(`Connected as ${client.user.tag}`)
@@ -26,6 +25,21 @@ client.once('disconnect', () => {
 
 client.login(bot_token)
 
+client.on('message', recivedMessage => commands.soundEffectsPlayer(recivedMessage))
+
 // -------------------------------------------------------------------------------------------------
 
-client.on('message', recivedMessage => commands.soundEffectsPlayer(recivedMessage))
+// Tive que fazer isso por conta de um erro no Heroku.
+// Nao consegui achar melhor solucao para isso no momento.
+// Erro em questao: Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch.
+
+const app = express();
+const port = 5001;
+
+app.get('/', async (req, res) => {
+    res.send('https://github.com/LucasGuerraCavalcante/xaropinho-bot/blob/main/README.md')
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// -------------------------------------------------------------------------------------------------
