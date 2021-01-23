bd3d64e55667a07d60bfcea757cd95a62290aeef
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-01-23 19:38:23 -0300

    comentario

---
+++ src/commands/commands.js
@@ -0,0 +1,225 @@
+
+const { prefix } = require('../utils/config.json');
+
+// Lista de comandos e funcionalidades de cada comando
+
+module.exports = {
+    "commandsArray": [
+        `${prefix}ajuda`,
+        `${prefix}uepa`,
+        `${prefix}pare`,
+        `${prefix}cavalo`,
+        `${prefix}atumalaca`,
+        `${prefix}aoow`,
+        `${prefix}rapaz`,
+        `${prefix}gato`,
+        `${prefix}irra`, // <--- Problema
+        `${prefix}queisso`, // <--- Problema
+        `${prefix}bombeiro`, 
+        `${prefix}camisa`,
+        `${prefix}aleluia`,
+        `${prefix}suspense`,
+        `${prefix}ronaldo`, // <--- Problema
+        `${prefix}patrao`,  // <--- Problema
+        `${prefix}non-ecziste`, 
+        `${prefix}danca`, // <--- Problema
+        `${prefix}brasil`,
+        `${prefix}corneta`, 
+        `${prefix}coracao`, 
+        `${prefix}aplausos`, 
+        `${prefix}vaias`, 
+        `${prefix}tarzan`, 
+        `${prefix}ratinho`,  // <--- Problema
+        `${prefix}papelao`,  // <--- Problema
+        `${prefix}rodaaroda`, // <--- Problema
+        `${prefix}perdeutudo`, // <--- Problema
+        `${prefix}telesena`,
+        `${prefix}piao-sbt`,
+        `${prefix}ai`, // <--- Problema
+        `${prefix}coice`,
+        `${prefix}brincadeira`,
+        `${prefix}chaves-xylophone`,
+        `${prefix}chaves-pancada`,  
+        `${prefix}chaves-risada`  
+    ],
+    "commandsData": [
+        {
+            "key": "ajuda"
+        },
+        {
+            "key": "uepa",
+            "message":"Uêpa!",
+            "url":"https://www.youtube.com/watch?v=gQm8hyMfz2o"
+        },
+        {
+            "key": "pare",
+            "message":"PARE!",
+            "url":"https://www.youtube.com/watch?v=djnyU9-ZyVg"
+        },
+        {
+            "key": "cavalo",
+            "message":"Cavalo!",
+            "url":"https://www.youtube.com/watch?v=rYd4Y4v9qmw"
+        },
+        {
+            "key": "atumalaca",
+            "message":"kkkkkkk",
+            "url":"https://www.youtube.com/watch?v=cVScsUqorGo"
+        },
+        {
+            "key": "aoow",
+            "message":"AOOOWW POTÊNCIA!!!",
+            "url":"https://www.youtube.com/watch?v=P5MphlfhOOA"
+        },
+        {
+            "key": "rapaz",
+            "message":"Rapaaaaiz",
+            "url":"https://www.youtube.com/watch?v=Jvl0L9GRH6o"
+        },
+        {
+            "key": "gato",
+            "message":"Miaaau",
+            "url":"https://www.youtube.com/watch?v=fTP2IbqMEtU"
+        },
+        {
+            "key": "irra",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=uaKKjT_d0jI"
+        },
+        {
+            "key": "queisso",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=ou3BlCaj1DM"
+        },
+        {
+            "key": "bombeiro",
+            "message":"Chama o bombeiro!!!",
+            "url":"https://www.youtube.com/watch?v=oOGOgM46-eE"
+        },
+        {
+            "key": "camisa",
+            "message":"TIRA A CAMISA!!!",
+            "url":"https://www.youtube.com/watch?v=2iqiFz_KGwU"
+        },
+        {
+            "key": "aleluia",
+            "message":"Aleluia!!!",
+            "url":"https://www.youtube.com/watch?v=ou-pUmCRgpY"
+        },
+        {
+            "key": "suspense",
+            "message":":O",
+            "url":"https://www.youtube.com/watch?v=GS7tHGWMDS4"
+        },
+        {
+            "key": "ronaldo",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=oabpxqp0zYQ"
+        },
+        {
+            "key": "patrao",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=KFdFEGpFXyg"
+        },
+        {
+            "key": "non-ecziste",
+            "message":"Isto Non Ecziste...",
+            "url":"https://www.youtube.com/watch?v=gDbiVwKSxdQ"
+        },
+        {
+            "key": "danca",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=OutIegT8HBg"
+        },
+        {
+            "key": "brasil",
+            "message":"BRASIL SIL SIL SIL....",
+            "url":"https://www.youtube.com/watch?v=EnLNDuKya04"
+        },
+        {
+            "key": "corneta",
+            "message":"....",
+            "url":"https://www.youtube.com/watch?v=s4rmbuQGdgQ"
+        },
+        {
+            "key": "coracao",
+            "message":"Tum tum tum...",
+            "url":"https://www.youtube.com/watch?v=z2fNFmbAVCI"
+        },
+        {
+            "key": "aplausos",
+            "message":"Clap clap clap...",
+            "url":"https://www.youtube.com/watch?v=ajJX8ZGXIxo"
+        },
+        {
+            "key": "vaias",
+            "message":"Bhuuu...",
+            "url":"https://www.youtube.com/watch?v=pvGtop2Sf-4"
+        },
+        {
+            "key": "tarzan",
+            "message":"OOOOooooOOooOOOOoo...",
+            "url":"https://www.youtube.com/watch?v=ttabWazASo0"
+        },
+        {
+            "key": "ratinho",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=-d6wB4KyuUk"
+        },
+        {
+            "key": "papelao",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=US9QhKiEg6w"
+        },
+        {
+            "key": "rodaaroda",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=KAle05huxKs"
+        },
+        {
+            "key": "perdeutudo",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=g2L1TJFALd4"
+        },
+        {
+            "key": "telesena",
+            "message":"É tele é tele é tele senaaaa! eu vou ganhar, na telesena!",
+            "url":"https://www.youtube.com/watch?v=UtKKg6wvW1I"
+        },
+        {
+            "key": "piao-sbt",
+            "message":"Pião da Casa Própria - Silvio Santos - SBT",
+            "url":"https://www.youtube.com/watch?v=f8qspc1aspE"
+        },
+        {
+            "key": "ai",
+            "message":"Infelizmente esse comando está temporariamente insdisponível",
+            "url":"https://www.youtube.com/watch?v=npYVf0gRgb0"
+        },
+        {
+            "key": "coice",
+            "message":"Coice do cavalo!",
+            "url":"https://www.youtube.com/watch?v=zTEBYSkJeOs"
+        },
+        {
+            "key": "brincadeira",
+            "message":"É brincadeira, hein!",
+            "url":"https://www.youtube.com/watch?v=C57kQv_hd40"
+        },
+        {
+            "key": "chaves-xylophone",
+            "message":"Chaves - Xylophone",
+            "url":"https://www.youtube.com/watch?v=JJ_lV4h_b9s"
+        },
+        {
+            "key": "chaves-pancada",
+            "message":"Chaves - Pancada",
+            "url":"https://www.youtube.com/watch?v=xUE_gSdUhZA"
+        },
+        {
+            "key": "chaves-risada",
+            "message":"kkkkkkkkkkkkkkkkk",
+            "url":"https://www.youtube.com/watch?v=Ujt1Q4M43UI"
+        }
+    ]
+}
